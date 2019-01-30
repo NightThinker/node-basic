18007a99a17f404f56e535e7dc0b3adae70c46f3
May <may@Gato-Pro.local>
2019-01-30 12:45:28 +0700

    Using the Node Modules System

--- app.js
+++ app.js
@@ -1,43 +1,9 @@
 const http = require('http');
-const fs = require('fs')
 
-const server = http.createServer((req, res) => {
-  // console.log(req.url, req.method, req.headers)
-  const url = req.url
-  const method = req.method
-  if(url === '/') {
-    res.write('<html>')
-    res.write('<head><title>Enter message</title></head>');
-    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>')
-    res.write('</html>')
-    return res.end()
-  }
+const routes = require('./routes')
 
-  if(url === '/message' && method === 'POST') {
-    const body = []
-    req.on('data', (chunk) => {
-			console.log('chunk', chunk)
-      body.push(chunk)
-    })
+console.log('TCL: routes', routes.someText)
 
-    return req.on('end', () => {
-      const paresedBody = Buffer.concat(body).toString()
-			// console.log('paresedBody', paresedBody)
-      const message = paresedBody.split('=')[1]
-			console.log('message : ', message)
-      fs.writeFile('message.txt', message, err => {
-        res.statusCode = 302
-        res.setHeader('Location', '/')
-        return res.end()
-      })
-    })
-  }
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<html>');
-  res.write('<head><title>My First Page</title></head>');
-  res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-  res.write('</html>');
-  res.end();
-})
+const server = http.createServer(routes.handler)
 
 server.listen(3000);
